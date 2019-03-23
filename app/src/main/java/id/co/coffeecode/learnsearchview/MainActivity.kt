593a432ff096abacbff2d039d80a25d59a059faf
Aulia Rahman <auliar68@gmail.com>
2019-03-23 15:56:35 +0700

    first init

---
+++ app/src/main/java/id/co/coffeecode/learnsearchview/MainActivity.kt
@@ -0,0 +1,14 @@
+package id.co.coffeecode.learnsearchview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
