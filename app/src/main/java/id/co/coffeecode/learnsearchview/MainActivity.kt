c2d9d1755ccc95e3453862c0ad07de99b5ff3985
Aulia Rahman <auliar68@gmail.com>
2019-03-25 10:54:54 +0700

    model 1 finish

--- app/src/main/java/id/co/coffeecode/learnsearchview/MainActivity.kt
+++ app/src/main/java/id/co/coffeecode/learnsearchview/MainActivity.kt
@@ -2,13 +2,53 @@ package id.co.coffeecode.learnsearchview
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.MenuItem
+import android.view.Menu
+import android.widget.ArrayAdapter
+import android.widget.SearchView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var arrayFood = ArrayList<String>()
+    internal var adapter: ArrayAdapter<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadData()
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayFood)
+
+        search_food.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.search_menu, menu)
+        val item = menu?.findItem(R.id.search_food)
+
+        val searchView = item?.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter?.filter?.filter(newText)
+                return false
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun loadData() {
+        arrayFood.clear()
+
+        val itemArray = resources.getStringArray(R.array.my_foods)
+        arrayFood.addAll(itemArray)
     }
 }
---
+++ app/src/main/java/id/co/coffeecode/learnsearchview/Sample.java
@@ -0,0 +1,8 @@
+package id.co.coffeecode.learnsearchview;
+
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+
+public class Sample {
+    ArrayAdapter<String> adapter;
+}
